1f0fbcb966384c7d1d907c6455fa48acc089b55f
dangernoob <screw.spam@gmail.com>
2022-11-30 20:22:59 +0000

    add vnet

--- terraform/data/dev-azure-bootcamp.tfvars
+++ terraform/data/dev-azure-bootcamp.tfvars
@@ -1,2 +1,4 @@
 bootcamp_resource_group_name1     = "dev-terraform-bootcamp"
 bootcamp_resource_group_location1 = "westus2"
+bootcamp_vnet_name = "dev-bootcampvNet"
+bootcamp_vnet_address_space = "10.0.0.0/24"
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "rg1" {
   name     = var.bootcamp_resource_group_name1
   location = var.bootcamp_resource_group_location1
 }
+
+resource "azurerm_virtual_network" "bootcampvNet" {
+  name                = var.bootcamp_vnet_name
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  address_space       = [var.bootcamp_vnet_address_space]
+}
