c678903e912e501d4d33f511b8bec6549ce54129
dangernoob <screw.spam@gmail.com>
2022-11-30 20:25:32 +0000

    add var decl

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,13 @@ variable "bootcamp_resource_group_location1" {
   type        = string
   description = "I am a resource group location"
 }
+
+variable "bootcamp_vnet_name" {
+  type        = string
+  description = "I am a vnet name for the bootcamp"
+}
+
+variable "bootcamp_vnet_address_space" {
+  type        = string
+  description = "I am a vnet address space"
+}
